d078eb4175171793c082595f68de981a717345f7
santoshkumarjena <santoshkumarjena@gmail.com>
2018-08-28 16:04:28 +0530

    change in the gitignore file

---
+++ src/main/java/com/itgd/eledata/EledataApplication.java
@@ -0,0 +1,12 @@
+package com.itgd.eledata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EledataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EledataApplication.class, args);
+	}
+}
